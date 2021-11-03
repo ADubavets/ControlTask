fde773d21ebfcd59e93796f9cae874bff96a9c0c
ADubavets <89544107+ADubavets@users.noreply.github.com>
2021-11-03 20:52:30 +0300

    Update SecondTask.java

--- SecondTask.java
+++ SecondTask.java
@@ -7,15 +7,11 @@ public class SecondTask {
         System.out.println("Дана строка:  " + s);
         StringBuilder sb = new StringBuilder(s);
         char symbol = sb.charAt(0);
-        int k = -1;
         for (int i = 1; i < sb.length(); i++) {
             int result = 1;
             for (int j = i; j < sb.length(); j++)
                 if (symbol == sb.charAt(j)) result++;
-            if (result == 1) {
-                k = i;
-                break;
-            }
+            if (result == 1) break;
             else symbol = sb.charAt(i);
         }
         System.out.println("Определен первый неповторяющийся символ в строке: " + symbol);
